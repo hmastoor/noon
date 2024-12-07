c01b4b2e7c42b0d11e86a94ad4ed0e9dcaf64537
Hassaan Mastoor <hassaanmastoor@gmail.com>
2024-12-06 19:34:32 -0500

    remove

--- llm_utils.py
+++ llm_utils.py
@@ -5,8 +5,7 @@ PRIMARY_MODEL = 'gpt-4o'
 MINI_MODEL = 'gpt-4o-mini'
 EMBEDDING_MODEL_LARGE = "text-embedding-3-large"
 EMBEDDING_MODEL_SMALL = "text-embedding-3-small"
-OPENAI_KEY = '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'
-# OPENAI_KEY = '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'
+OPENAI_KEY = '123'
 
 # Initialize OpenAI client
 openai_client = OpenAI(api_key=OPENAI_KEY)
